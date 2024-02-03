36a7a713ba73842378937940b7dad31883479a85
Esmail Shabayek <esmail.shabayek@gmail.com>
2024-02-03 19:58:39 +0200

    Fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,10 +11,12 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
+
 @app_views.route('/status', strict_slashes=False)
 def ok():
     '''return stuff'''
     return jsonify(status='OK')
+
 
 @app_views.route('/stats', methods=['GET'])
 def stats():
