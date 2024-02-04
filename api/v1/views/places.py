a7d292bfafffd1af10a4ba5a6e2e8d75a25e6c4f
Esmail Shabayek <esmail.shabayek@gmail.com>
2024-02-04 23:05:50 +0200

    Add places curd apis

---
+++ api/v1/views/places.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""
+Flask route that returns json status response
+"""
+from api.v1.views import app_views
+from flask import abort, jsonify, make_response, request
+from models import storage
+from models.place import Place
+
+
+@app_views.route('/cities/<city_id>/places/', methods=['GET', 'POST'], strict_slashes=False)
+def get_places(city_id):
+    """
+        states route to handle http method for requested state/s
+    """
+    cities = storage.all('City')
+    fetch_string = "{}.{}".format('City', city_id)
+    city = cities.get(fetch_string)
+
+    print(city)
+    if city is None:
+        abort(401)
+
+    if request.method == 'GET':
+        places = []
+        for place in city.places:
+            places.append(place.to_dict())
+        return jsonify(places)
+
+    if request.method == 'POST':
+        if not request.get_json():
+            return make_response(jsonify({'error': 'Not a JSON'}), 400)
+        if 'user_id' not in request.get_json():
+            return make_response(jsonify({'error': 'Missing user_id'}), 400)
+        if 'name' not in request.get_json():
+            return make_response(jsonify({'error': 'Missing name'}), 400)
+        new_place = Place(**request.get_json())
+        new_place.save()
+        return make_response(jsonify(new_place.to_dict()), 201)
+
+
+@app_views.route('/places/<place_id>', methods=['GET', 'DELETE', 'PUT'],
+                 strict_slashes=False)
+def get_place(place_id):
+    """
+        states route to handle http methods for requested state
+    """
+    places = storage.all('Place')
+    fetch_string = "{}.{}".format('Place', place_id)
+    place = places.get(fetch_string)
+
+    if place is None:
+        abort(404)
+
+    if request.method == 'GET':
+        return jsonify(place.to_dict())
+
+    if request.method == 'DELETE':
+        place.delete()
+        storage.save()
+        return jsonify({}), 200
+
+    if request.method == 'PUT':
+        if not request.get_json():
+            return make_response(jsonify({'error': 'Not a JSON'}), 400)
+        for key, value in request.get_json().items():
+            if key not in ['id', 'created_at', 'updated_at', 'user_id', 'city_id']:
+                setattr(place, key, value)
+        place.save()
+        return jsonify(place.to_dict())
