c475b35577816d4f676fb52f9572db0c2b47cde7
Esmail Shabayek <esmail.shabayek@gmail.com>
2024-02-07 00:46:24 +0200

    Fix base model

--- models/base_model.py
+++ models/base_model.py
@@ -68,7 +68,7 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
-        if new_dict['__class__'] is "User" and not save_to_disk:
+        if new_dict['__class__'] == "User" and not save_to_disk:
             new_dict.pop("_password", None)
         return new_dict
 
